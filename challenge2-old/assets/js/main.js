d681119c9de714173f8e8779ce44a716ac9e4172
Maria Luiza Vidal <marialuizavidal01@gmail.com>
2022-10-12 15:31:06 -0300

    fetch changes corrected

--- challenge2-old/assets/js/main.js
+++ challenge2-old/assets/js/main.js
@@ -1,153 +1,5 @@
-// const data = {
-//     "insurances": [{
-//         "id": 3322,
-//         "name": "Amil"
-//     }, {
-//         "id": 3293,
-//         "name": "Bradesco"
-//     }, {
-//         "id": 99231,
-//         "name": "Hapvida"
-//     }, {
-//         "id": 1322,
-//         "name": "CASSI"
-//     }, {
-//         "id": 23111,
-//         "name": "Sulamérica"
-//     }],
-//     "guides": [{
-//         "number": "3210998321",
-//         "start_date": "2022-09-23T19:18:47.210Z",
-//         "patient": {
-//             "id": 9321123,
-//             "name": "Augusto Ferreira",
-//             "thumb_url": "https://imgsapp2.correiobraziliense.com.br/app/noticia_127983242361/2019/10/09/794834/20191004154953157610i.jpg"
-//         },
-//         "insurane_id": 1322,
-//         "health_insurance": {
-//             "id": 1322,
-//             "name": "CASSI",
-//             "is_deleted": false
-//         },
-//         "price": 5567.2
-//     }, {
-//         "number": "287312832",
-//         "start_date": "2022-09-23T19:18:47.210Z",
-//         "patient": {
-//             "id": 93229123,
-//             "name": "Caio Carneiro",
-//             "thumb_url": "http://3.bp.blogspot.com/-XG5bGlqGnJw/T9lIcssnybI/AAAAAAAADTA/B23ezXOkx8Y/s1600/Aang.jpg"
-//         },
-//         "insurane_id": 1322,
-//         "health_insurance": {
-//             "id": 1322,
-//             "name": "CASSI",
-//             "is_deleted": false
-//         },
-//         "price": 213.3
-//     }, {
-//         "number": "283718273",
-//         "start_date": "2022-09-22T19:18:47.210Z",
-//         "patient": {
-//             "id": 213122388,
-//             "name": "Luciano José",
-//             "thumb_url": "https://i.ytimg.com/vi/yUXd-enstO8/maxresdefault.jpg"
-//         },
-//         "insurane_id": 3293,
-//         "health_insurance": {
-//             "id": 3293,
-//             "name": "Bradesco",
-//             "is_deleted": true
-//         },
-//         "price": 88.99
-//     }, {
-//         "number": "009090321938",
-//         "start_date": "2022-09-20T19:18:47.210Z",
-//         "patient": {
-//             "id": 3367263,
-//             "name": "Felício Santos",
-//             "thumb_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPSxlYabmRlKk43uvsBMIqjA7Rw_YCwK4TyA&usqp=CAU"
-//         },
-//         "insurane_id": 3293,
-//         "health_insurance": {
-//             "id": 3293,
-//             "name": "Bradesco",
-//             "is_deleted": true
-//         },
-//         "price": 828.99
-//     }, {
-//         "number": "8787128731",
-//         "start_date": "2022-09-01T19:18:47.210Z",
-//         "patient": {
-//             "id": 777882,
-//             "name": "Fernando Raposo"
-//         },
-//         "insurane_id": 3322,
-//         "health_insurance": {
-//             "id": 3322,
-//             "name": "Amil",
-//             "is_deleted": false
-//         },
-//         "price": 772
-//     }, {
-//         "number": "12929321",
-//         "start_date": "2022-09-02T19:18:47.210Z",
-//         "patient": {
-//             "id": 221,
-//             "name": "Paciente com nome grante pra colocar text ellipsis testando nome com paciente grande"
-//         },
-//         "insurane_id": 3322,
-//         "health_insurance": {
-//             "id": 3322,
-//             "name": "Amil",
-//             "is_deleted": false
-//         },
-//         "price": 221
-//     }]
-// }
-
-fetch('https://augustoferreira.com/augustoferreira/amigo/guides.json').then((data) => {
-    console.log(data.json())
-    return data.json();
-}).then((completeData) => {
-    let guides = '';
-    completeData.map((values) => {
-        guides = values.data;
-        console.log(values.data)
-    })
-})
-
-// let resp;
-// const getUsers = async () => {
-//   const url = 'https://augustoferreira.com/augustoferreira/amigo/guides.json';
-//   try {
-//       const res = await fetch(url);
-//       return await res.json()
-//   } catch (error) {
-//       console.log(error);
-//   }
-// }
-// const initial = async () => {
-//   resp = await getUsers();
-//   console.log(resp.data)
-//   renderTable(resp.data);
-// }
-// initial(); 
-
-// const getInsurances = async () => {
-//   const url = 'https://augustoferreira.com/augustoferreira/amigo/guides.json';
-//   try {
-//       const res = await fetch(url);
-//       return await res.json()
-//   } catch (error) {
-//       console.log(error);
-//   }
-// }
-// const initial = async () => {
-//   data = await getInsurances();
-//   renderTable(data);
-// }
-// initial();
+const urlGuides = 'https://augustoferreira.com/augustoferreira/amigo/guides.json'
+const urlInsurances = 'https://augustoferreira.com/augustoferreira/amigo/insurances.json'
 
 const table = document.getElementById('table');
 const tbody = document.getElementById('tbody');
@@ -157,13 +9,21 @@ const dateStartInput = document.getElementById('month-start');
 const dateEndInput = document.getElementById('month-end');
 
 const itemsPerPage = 2;
-// const { insurances, guides } = data;
 
 let currentPage = 1;
 let filteredGuides;
 let guidesAmount;
+let guides = [];
+let insurances;
 
-const init = () => {
+const init = async () => {
+    const [{ data: getGuidesResponse }, { data: insurances }] = await Promise.all([
+        getFetch(urlGuides),
+        getFetch(urlInsurances)
+    ]);
+
+    guides = getGuidesResponse.guides;
+
     changeDateFilter('month');
     renderTable(guides);
     renderSelectedInsurances(insurances);
@@ -171,10 +31,17 @@ const init = () => {
     paginationStructure(filteredGuides, 2);
 };
 
-const renderTable = guides => {
-    let html = '';
+const getFetch = async url => {
+    const response = await fetch(url)
+    return await response.json()
+}
 
-    if (!guides.length) {
+const renderTable = guidesParams => {
+    let html = '';
+    filteredGuides = guidesParams;
+
+    console.log(guidesParams)
+    if (!guidesParams.length) {
         html += `
         <tr>
         <td colspan="5" id="footer">Nenhuma guia encontrada</td>
@@ -182,16 +49,18 @@ const renderTable = guides => {
         `
     }
 
-    guides.forEach(guide => {
-        const healthInsuranceClass = guide.health_insurance.is_deleted ? 'class="deleted" title="Convênio apagado"' : '';
+    guidesParams.forEach(async guide => {
+        const priceValue = guide.price === 'NaN' ? 0 : guide.price;
+        const healtinsuranceName = guide && guide.health_insurance && guide.health_insurance.name ? guide.health_insurance.name : '';
+        const healthInsuranceClass = guide && guide.health_insurance && guide.health_insurance.is_deleted ? 'class="deleted" title="Convênio apagado"' : '';
 
         html += `
         <tr>
             <td>${new Date(guide.start_date).toLocaleDateString('pt-BR')}</td>
-            <td>${guide.number}</td>
+            <td>${guide.number || '-'}</td>
             <td><img id="img" src="${guide.patient.thumb_url || "https://via.placeholder.com/150x150.jpg"}"/>${guide.patient.name}</td>
-            <td ${healthInsuranceClass}>${guide.health_insurance.name}</td>
-            <td>${guide.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</td>
+            <td ${healthInsuranceClass}>${healtinsuranceName}</td>
+            <td>${priceValue.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</td>
         </tr>
         `
     });
@@ -199,7 +68,8 @@ const renderTable = guides => {
     tbody.innerHTML = html;
 };
 
-const renderSelectedInsurances = insurances => {
+const renderSelectedInsurances = insurancesParams => {
+    insurances = insurancesParams;
     let html = `<option value="">Convênio</option>`;
 
     insurances.forEach(insurance => {
@@ -252,8 +122,8 @@ const paginate = (page = 1, filtered) => {
     currentPage = page;
 
     const offset = (currentPage - 1) * itemsPerPage;
-    const slicedItemsWithOffset = filtered.slice(offset);
-    const paginatedGuides = slicedItemsWithOffset.slice(0, itemsPerPage);
+    const slicedItemsWithOffset = filtered && filtered.slice(offset);
+    const paginatedGuides = slicedItemsWithOffset && slicedItemsWithOffset.slice(0, itemsPerPage);
 
     paginationStructure(filteredGuides, 2);
     renderTable(paginatedGuides);
@@ -325,7 +195,7 @@ const filterTable = (currentPage) => {
             isValid = false;
         }
 
-        if (selectValue && element.health_insurance.id !== selectValue) {
+        if (selectValue && element.health_insurance?.id !== selectValue) {
             isValid = false;
         }
 
@@ -352,6 +222,8 @@ const filterTable = (currentPage) => {
 
         return 0;
     });
+
+    // renderTable(paginatedGuides);
 
     paginate(currentPage, filteredGuides);
 };
